f5aa4e6950aa2b53f093a1c6b8cb8d7fd96ecda1
timchen <timchen10001@gmail.com>
2023-09-29 20:12:10 +0800

    feat(routing): GET hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn hello() -> &'static str {
+    "Hello World\n1"
+}
+
+#[rocket::main]
+async fn main() {
+    let _ = rocket::build().mount("/", routes![hello]).launch().await;
 }
