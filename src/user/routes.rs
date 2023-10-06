648a31f391c763848420a4f6ab8e1ae75e8eeb82
timchen <timchen10001@gmail.com>
2023-10-06 18:40:56 +0800

    feat(db): error handling

--- src/user/routes.rs
+++ src/user/routes.rs
@@ -5,7 +5,8 @@ use crate::{
     DbConn,
 };
 use rocket::{
-    response::status,
+    http::Status,
+    response::status::{self, Custom},
     serde::json::{Json, Value},
 };
 use serde_json::json;
@@ -16,29 +17,35 @@ pub fn hello() -> Value {
 }
 
 #[get("/user")]
-pub async fn get_users(_auth: BasicAuth, con: DbConn) -> Value {
+pub async fn get_users(__rocket_auth: BasicAuth, con: DbConn) -> Result<Value, Custom<Value>> {
     con.run(|c| {
-        let user = UserRepository::find_all(c, 1000).expect("Query user failed");
-        json!(user)
+        UserRepository::find_all(c, 1000)
+            .map(|users| json!(users))
+            .map_err(|_| Custom(Status::InternalServerError, json!("Something went wrong.")))
     })
     .await
 }
 
 #[get("/user/<id>")]
-pub async fn get_user(id: i32, _auth: BasicAuth, db: DbConn) -> Value {
+pub async fn get_user(
+    id: i32,
+    __rocket_auth: BasicAuth,
+    db: DbConn,
+) -> Result<Value, Custom<Value>> {
     db.run(move |c| {
-        let user = UserRepository::find(c, id).expect("DB error querying");
-        json!(user)
+        UserRepository::find(c, id)
+            .map(|user| json!(user))
+            .map_err(|_| Custom(Status::NotFound, json!("User not found.")))
     })
     .await
 }
 
 #[post("/user", format = "json", data = "<new_user>")]
-pub async fn create_user(db: DbConn, new_user: Json<CreateUserRq>) -> Value {
+pub async fn create_user(db: DbConn, new_user: Json<CreateUserRq>) -> Result<Value, Custom<Value>> {
     db.run(|c| {
-        let created_user =
-            UserRepository::create(c, new_user.into_inner()).expect("DB error inserting");
-        json!(created_user)
+        UserRepository::create(c, new_user.into_inner())
+            .map(|created_user| json!(created_user))
+            .map_err(|_| Custom(Status::InternalServerError, json!("Create User Failed.")))
     })
     .await
 }
@@ -47,22 +54,27 @@ pub async fn create_user(db: DbConn, new_user: Json<CreateUserRq>) -> Value {
 pub async fn update_user(
     id: i32,
     db: DbConn,
-    _auth: BasicAuth,
+    __rocket_auth: BasicAuth,
     update_user: Json<UpdateUserRq>,
-) -> Value {
+) -> Result<Value, Custom<Value>> {
     db.run(move |c| {
-        let user =
-            UserRepository::save(c, id, update_user.into_inner()).expect("DB error updating");
-        json!(user)
+        UserRepository::save(c, id, update_user.into_inner())
+            .map(|updated_user| json!(updated_user))
+            .map_err(|_| Custom(Status::InternalServerError, json!("Update user failed.")))
     })
     .await
 }
 
 #[delete("/user/<id>", format = "json")]
-pub async fn delete_user(id: i32, _auth: BasicAuth, db: DbConn) -> status::NoContent {
+pub async fn delete_user(
+    id: i32,
+    __rocket_auth: BasicAuth,
+    db: DbConn,
+) -> Result<status::NoContent, Custom<Value>> {
     db.run(move |c| {
-        UserRepository::delete(c, id).expect("DB error deleting");
-        status::NoContent
+        UserRepository::delete(c, id)
+            .map(|_| status::NoContent)
+            .map_err(|_| Custom(Status::InternalServerError, json!("Delete user failed.")))
     })
     .await
 }
