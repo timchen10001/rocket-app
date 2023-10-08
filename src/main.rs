4b3234aae9beaa0328e5ebad0fb83ad594282e60
timchen <timchen10001@gmail.com>
2023-10-08 12:07:24 +0800

    feat(db): diesel migrations automations

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,30 @@ mod models;
 mod schema;
 mod user;
 
+// use diesel_migrations::EmbeddedMigrations;
 use error::*;
+use rocket::{fairing::AdHoc, Build, Rocket};
 use rocket_sync_db_pools::database;
 use user::routes::*;
 
 #[database("sqlite")]
 pub struct DbConn(diesel::SqliteConnection);
+
+async fn run_db_migrations(rocket: Rocket<Build>) -> Rocket<Build> {
+    use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
+
+    const MIGRATIONS: EmbeddedMigrations = embed_migrations!();
+
+    DbConn::get_one(&rocket)
+        .await
+        .expect("Unable to retrieve connection")
+        .run(|c| {
+            c.run_pending_migrations(MIGRATIONS).expect("Migrations failed");
+        })
+        .await;
+
+    rocket
+}
 
 #[rocket::main]
 async fn main() {
@@ -33,6 +51,7 @@ async fn main() {
             catchers![not_found, unauthorized, unprocessable_entity],
         )
         .attach(DbConn::fairing())
+        .attach(AdHoc::on_ignite("Diesel migrations", run_db_migrations))
         .launch()
         .await;
 }
